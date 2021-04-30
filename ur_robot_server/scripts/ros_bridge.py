25f9d250922b6ef9b55fa7e9d0a5ce1b5d540cc4
L-eonor <ronoelsantos@gmail.com>
2021-04-30 17:36:46 +0100

    removed unused

--- ur_robot_server/scripts/ros_bridge.py
+++ ur_robot_server/scripts/ros_bridge.py
@@ -363,12 +363,10 @@ class UrRosBridge:
         else:
             self.collision_sensors["wrist_3"]=True
 
-
 class UrGripperRosBridge:
 
     def __init__(self,  real_robot=False, ur_model = 'ur5', number_of_joint_positions=7, number_of_joint_velocities=7):
         #number of joint positions and velocities-> gripper has no velocity sensor?
-        self.target_len=6 #(xyxrpy)
         self.number_of_joint_positions=number_of_joint_positions
         self.number_of_joint_velocities=number_of_joint_velocities
 
@@ -383,13 +381,6 @@ class UrGripperRosBridge:
         # joint_trajectory_command_handler publisher
         self.arm_cmd_pub = rospy.Publisher('env_arm_command', JointTrajectory, queue_size=1)
 
-        # Target RViz Marker publisher
-        self.target_pub = rospy.Publisher('target_marker', Marker, queue_size=10)
-
-        # Obstacle controller publisher
-        self.obstacle_controller_pub = rospy.Publisher('move_obstacle', Bool, queue_size=10)
-
-        self.target = [0.0] * 6
         self.ur_state = [0.0] * (self.number_of_joint_positions + self.number_of_joint_velocities) #(7 ur_joint_pos + 6 ur_joint_vel)
 
         rospy.Subscriber("joint_states", JointState, self.callbackUR)
@@ -402,8 +393,6 @@ class UrGripperRosBridge:
         self.control_period = rospy.Duration.from_sec(self.sleep_time)
 
         self.reference_frame = rospy.get_param("~reference_frame", "base")
-        self.ee_frame = 'tool0'
-        self.target_frame = 'target'
 
 
         self.max_velocity_scale_factor = float(rospy.get_param("~max_velocity_scale_factor"))
@@ -436,12 +425,6 @@ class UrGripperRosBridge:
         #TODO
         self.safe_to_move = True
 
-        self.obstacle_controller = rospy.get_param("~obstacle_controller", False)
-
-        # Target mode
-        self.target_mode = rospy.get_param("~target_mode", 'fixed')
-        self.target_model_name = rospy.get_param("~target_model_name", 'box100')
-
         #object(cubes) handler
         self.cubes_controller=ObjectsController()
          
@@ -452,28 +435,8 @@ class UrGripperRosBridge:
         self.get_state_event.clear()
         # Get environment state
         state =[]
-        if self.target_mode == 'fixed':
-            target = copy.deepcopy(self.target)
-        elif self.target_mode == 'moving':
-            if self.real_robot:
-                (t_position, t_quaternion) = self.tf_listener.lookupTransform(self.reference_frame,self.target_frame,rospy.Time(0))
-                target = t_position + [0,0,0]
-            else:
-                pose = self.get_model_state_pose(self.target_model_name)
-
-                # Convert orientation target from Quaternion to RPY
-                quaternion = PyKDL.Rotation.Quaternion(pose[3],pose[4],pose[5],pose[6])
-                r,p,y = quaternion.GetRPY()
-                target = pose[0:3] + [r,p,y]
-               
-        else: 
-            raise ValueError
-            
+                         
         ur_state = copy.deepcopy(self.ur_state)
-
-        (position, quaternion) = self.tf_listener.lookupTransform(self.reference_frame,self.ee_frame,rospy.Time(0))
-
-        ee_to_base_transform = position + quaternion
 
         if self.real_robot:
             ur_collision = False
@@ -492,9 +455,7 @@ class UrGripperRosBridge:
 
         # Create and fill State message
         msg = robot_server_pb2.State()
-        msg.state.extend(target)
         msg.state.extend(ur_state)
-        msg.state.extend(ee_to_base_transform)
         msg.state.extend([ur_collision])
         msg.state.extend(gripper_pose)
 
@@ -509,42 +470,12 @@ class UrGripperRosBridge:
         state = state_msg.state 
         # Clear reset Event
         self.reset.clear()
-        # Set target internal value
-        if self.target_mode == 'fixed':
-            self.target = copy.deepcopy(state[0:self.target_len]) #0:6
-            # Publish Target Marker
-            self.publish_target_marker(self.target)
-        # Stop movement of obstacles
-        if self.obstacle_controller:
-            msg = Bool()
-            msg.data = False
-            self.obstacle_controller_pub.publish(msg)
-            if state_msg.string_params["function"] == "triangle_wave":
-                rospy.set_param("target_function", "triangle_wave")
-                rospy.set_param("x", state_msg.float_params["x"])
-                rospy.set_param("y", state_msg.float_params["y"])
-                rospy.set_param("z_amplitude", state_msg.float_params["z_amplitude"])
-                rospy.set_param("z_frequency", state_msg.float_params["z_frequency"])
-                rospy.set_param("z_offset",    state_msg.float_params["z_offset"])
-            elif state_msg.string_params["function"] == "3d_spline":
-                rospy.set_param("target_function", "3d_spline")
-                rospy.set_param("x_min", state_msg.float_params["x_min"])
-                rospy.set_param("x_max", state_msg.float_params["x_max"])
-                rospy.set_param("y_min", state_msg.float_params["y_min"])
-                rospy.set_param("y_max", state_msg.float_params["y_max"])
-                rospy.set_param("z_min", state_msg.float_params["z_min"])
-                rospy.set_param("z_max", state_msg.float_params["z_max"])
-                rospy.set_param("n_points", state_msg.float_params["n_points"])
-                rospy.set_param("n_sampling_points", state_msg.float_params["n_sampling_points"])
-
+        
         # UR Joints Positions
         reset_steps = int(15.0/self.sleep_time)
         for i in range(reset_steps):
-            position_cmd=state[self.target_len:(self.target_len + self.number_of_joint_positions) ]
+            position_cmd=state[0:self.number_of_joint_positions ]
             self.publish_env_arm_cmd_without_gripper(position_cmd) #6:(6+7) =6:13
-
-        #updates gripper after arm reaches final position
-        #self.open_close_gripper(joint_value=joint_value)
 
         #initializes gripper-> fully open
         self.gripper_controller.init_gripper()
@@ -552,19 +483,13 @@ class UrGripperRosBridge:
         if not self.real_robot:
             # Reset collision sensors flags
             self.collision_sensors.update(dict.fromkeys(["shoulder","upper_arm","forearm","wrist_1","wrist_2","wrist_3"], False))
-        # Start movement of obstacles
-        if self.obstacle_controller:
-            msg = Bool()
-            msg.data = True
-            self.obstacle_controller_pub.publish(msg)
-
+        
         #object(cubes) position reset
         self.cubes_controller.reset_cubes_pos()
 
         self.reset.set()
 
         return 1
-
 
     def publish_env_arm_cmd_without_gripper(self, position_cmd):
         """Publish environment JointTrajectory msg.
@@ -579,37 +504,33 @@ class UrGripperRosBridge:
 
         """
 
-        if self.safe_to_move:
-            msg = JointTrajectory()
-            msg.header = Header()
-            msg.joint_names = ["elbow_joint", "shoulder_lift_joint", "shoulder_pan_joint", \
-                                "wrist_1_joint", "wrist_2_joint", "wrist_3_joint"]
-            msg.points=[JointTrajectoryPoint()]
 
-            #removes finger_joint (index 1)
-            joint_value=position_cmd[1]
-            position_cmd= copy.deepcopy(position_cmd)
-            del position_cmd[1]
-            
-            msg.points[0].positions = position_cmd
-            dur = []
-            for i in range(len(msg.joint_names)):
-                # !!! Be careful here with ur_state index
-                pos = self.ur_state[i]
-                cmd = position_cmd[i]
-                max_vel = self.ur_joint_vel_limits[i]
-                dur.append(max(abs(cmd-pos)/max_vel,self.min_traj_duration))
+        msg = JointTrajectory()
+        msg.header = Header()
+        msg.joint_names = ["elbow_joint", "shoulder_lift_joint", "shoulder_pan_joint", \
+                            "wrist_1_joint", "wrist_2_joint", "wrist_3_joint"]
+        msg.points=[JointTrajectoryPoint()]
 
-            msg.points[0].time_from_start = rospy.Duration.from_sec(max(dur))
-            self.arm_cmd_pub.publish(msg)
-            
-            rospy.sleep(self.control_period)
-            return position_cmd
-        else:
-            rospy.sleep(self.control_period)
-            return [0.0]*6
+        #removes finger_joint (index 1)
+        joint_value=position_cmd[1]
+        position_cmd= copy.deepcopy(position_cmd)
+        del position_cmd[1]
+        
+        msg.points[0].positions = position_cmd
+        dur = []
+        for i in range(len(msg.joint_names)):
+            # !!! Be careful here with ur_state index
+            pos = self.ur_state[i]
+            cmd = position_cmd[i]
+            max_vel = self.ur_joint_vel_limits[i]
+            dur.append(max(abs(cmd-pos)/max_vel,self.min_traj_duration))
 
-    
+        msg.points[0].time_from_start = rospy.Duration.from_sec(max(dur))
+        self.arm_cmd_pub.publish(msg)
+        
+        rospy.sleep(self.control_period)
+        return position_cmd
+
     def publish_env_arm_cmd(self, position_cmd):
         """Publish environment JointTrajectory msg.
 
@@ -623,38 +544,35 @@ class UrGripperRosBridge:
 
         """
 
-        if self.safe_to_move:
-            msg = JointTrajectory()
-            msg.header = Header()
-            msg.joint_names = ["elbow_joint", "shoulder_lift_joint", "shoulder_pan_joint", \
-                                "wrist_1_joint", "wrist_2_joint", "wrist_3_joint"]
-            msg.points=[JointTrajectoryPoint()]
 
-            #removes finger_joint (index 1)
-            joint_value=position_cmd[1]
-            position_cmd= copy.deepcopy(position_cmd)
-            del position_cmd[1]
-            
-            msg.points[0].positions = position_cmd
-            dur = []
-            for i in range(len(msg.joint_names)):
-                # !!! Be careful here with ur_state index
-                pos = self.ur_state[i]
-                cmd = position_cmd[i]
-                max_vel = self.ur_joint_vel_limits[i]
-                dur.append(max(abs(cmd-pos)/max_vel,self.min_traj_duration))
+        msg = JointTrajectory()
+        msg.header = Header()
+        msg.joint_names = ["elbow_joint", "shoulder_lift_joint", "shoulder_pan_joint", \
+                            "wrist_1_joint", "wrist_2_joint", "wrist_3_joint"]
+        msg.points=[JointTrajectoryPoint()]
 
-            msg.points[0].time_from_start = rospy.Duration.from_sec(max(dur))
-            self.arm_cmd_pub.publish(msg)
-            
-            #send gripper command
-            self.open_close_gripper(joint_value=joint_value)
+        #removes finger_joint (index 1)
+        joint_value=position_cmd[1]
+        position_cmd= copy.deepcopy(position_cmd)
+        del position_cmd[1]
+        
+        msg.points[0].positions = position_cmd
+        dur = []
+        for i in range(len(msg.joint_names)):
+            # !!! Be careful here with ur_state index
+            pos = self.ur_state[i]
+            cmd = position_cmd[i]
+            max_vel = self.ur_joint_vel_limits[i]
+            dur.append(max(abs(cmd-pos)/max_vel,self.min_traj_duration))
 
-            rospy.sleep(self.control_period)
-            return position_cmd
-        else:
-            rospy.sleep(self.control_period)
-            return [0.0]*6
+        msg.points[0].time_from_start = rospy.Duration.from_sec(max(dur))
+        self.arm_cmd_pub.publish(msg)
+        
+        #send gripper command
+        self.open_close_gripper(joint_value=joint_value)
+
+        rospy.sleep(self.control_period)
+        return position_cmd
 
     def open_close_gripper(self, joint_value):
         if joint_value == 0:
@@ -663,47 +581,6 @@ class UrGripperRosBridge:
             self.gripper_controller.close_gripper()
         else:
             self.gripper_controller.command_gripper(joint_value)
-
-    def get_model_state(self, model_name, relative_entity_name=''):
-        # method used to retrieve model state from gazebo simulation
-
-        rospy.wait_for_service('/gazebo/get_model_state')
-        try:
-            model_state = rospy.ServiceProxy('/gazebo/get_model_state', GetModelState)
-            model_coordinates = model_state(model_name, relative_entity_name)
-            x = model_coordinates.pose.position.x
-            y = model_coordinates.pose.position.y
-
-            orientation = PyKDL.Rotation.Quaternion(model_coordinates.pose.orientation.x,
-                                                 model_coordinates.pose.orientation.y,
-                                                 model_coordinates.pose.orientation.z,
-                                                 model_coordinates.pose.orientation.w)
-
-            euler_orientation = orientation.GetRPY()
-            yaw = euler_orientation[2]
-
-            x_vel = model_coordinates.twist.linear.x
-            y_vel = model_coordinates.twist.linear.y
-            yaw_vel = model_coordinates.twist.angular.z
-
-            return [x, y, yaw, x_vel, y_vel, yaw_vel]
-        except rospy.ServiceException as e:
-            print("Service call failed:" + e)
-    
-    def get_model_state_pose(self, model_name, relative_entity_name=''):
-        # method used to retrieve model pose from gazebo simulation
-
-        rospy.wait_for_service('/gazebo/get_model_state')
-        try:
-            model_state = rospy.ServiceProxy('/gazebo/get_model_state', GetModelState)
-            s = model_state(model_name, relative_entity_name)
-
-            pose = [s.pose.position.x, s.pose.position.y, s.pose.position.z, \
-                    s.pose.orientation.x, s.pose.orientation.y, s.pose.orientation.z, s.pose.orientation.w]
-
-            return pose
-        except rospy.ServiceException as e:
-            print("Service call failed:" + e)
 
     def get_link_state(self, link_name, reference_frame=''):
         # method used to retrieve link state from gazebo simulation
@@ -736,32 +613,6 @@ class UrGripperRosBridge:
             return x, y, z, roll, pitch, yaw, x_vel, y_vel, z_vel, roll_vel, pitch_vel,  yaw_vel
         except rospy.ServiceException as e:
             print("Service call failed:" + e)
-
-    def publish_target_marker(self, target_pose):
-        t_marker = Marker()
-        t_marker.type=1 #=>CUBE
-        t_marker.action=0
-        t_marker.frame_locked=1
-        t_marker.pose.position.x=target_pose[0]
-        t_marker.pose.position.y=target_pose[1]
-        t_marker.pose.position.z=target_pose[2]
-        rpy_orientation = PyKDL.Rotation.RPY(target_pose[3],target_pose[4],target_pose[5])
-        q_orientation = rpy_orientation.GetQuaternion()
-        t_marker.pose.orientation.x = q_orientation[0]
-        t_marker.pose.orientation.y = q_orientation[1]
-        t_marker.pose.orientation.z = q_orientation[2]
-        t_marker.pose.orientation.w = q_orientation[3]
-        t_marker.scale.x=0.1
-        t_marker.scale.y=0.1
-        t_marker.scale.z=0.1
-        t_marker.id=0
-        t_marker.header.stamp=rospy.Time.now()
-        t_marker.header.frame_id= self.reference_frame
-        t_marker.color.a=0.7
-        t_marker.color.r=1.0  # red
-        t_marker.color.g=0.0
-        t_marker.color.b=0.0
-        self.target_pub.publish(t_marker)
 
     def callbackUR(self,data):
         if self.get_state_event.is_set():
